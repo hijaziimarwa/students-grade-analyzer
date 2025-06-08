6b1567e85d495ebdd9e4f9c3dc7cf68a798c0b2c
hijaziimarwa <hijaziimarwa@gmail.com>
2025-06-08 17:48:21 +0300

    fix: add validation to ensure grade is <≤ 100

--- stud-grades.py
+++ stud-grades.py
@@ -6,3 +6,8 @@ for i in range(nb_of_students):
     name = input("Enter the student's name: ")
     grade = int(input("Enter the student's grade: "))
     
+    if grade <= 100:
+        grades_list.append(grade)
+        names_list.append(name)
+    else:
+        print('Please input a grade below 100')
