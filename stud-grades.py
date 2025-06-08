73eb266efcfc9dac515e59ebd8585f3128c0c910
hijaziimarwa <hijaziimarwa@gmail.com>
2025-06-08 17:47:45 +0300

    feat: prompt user for number of students, names, and grades

---
+++ stud-grades.py
@@ -0,0 +1,8 @@
+nb_of_students = int(input("Please enter the number of the students: "))
+names_list = []
+grades_list = []
+
+for i in range(nb_of_students):
+    name = input("Enter the student's name: ")
+    grade = int(input("Enter the student's grade: "))
+    
